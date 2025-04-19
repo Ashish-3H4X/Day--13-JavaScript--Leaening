83f3549cef73948f9bb4b496810b2993d6ebe468
Mr Rahul <ashish227087@gmail.com>
2025-04-19 20:22:59 +0530

    all String method problem  by gpt question solution

---
+++ project1/script.js
@@ -0,0 +1,36 @@
+//  sting reverse  using javaScript 
+
+  let text ="I love Codding"
+
+   console.log(text.split('').reverse().join(''))
+
+/// another method for reverse 
+  let str ="luhaR"
+   let reversed = '';
+
+   for (let i = str.length - 1; i >= 0; i--) {
+
+     reversed += str[i];
+     
+     
+    }console.log(reversed);
+
+  ///  check A string is palidrome  string ? 
+
+   const checkPalidromeString = (strCheck)=>{
+
+      let reversedString ='';
+      for( let i = strCheck.length -1 ; i>=0; i--){
+
+         reversedString += strCheck[i];
+
+         };
+        
+         if(reversedString === strCheck){
+          console.log("String is Palidrome String")
+      } else {
+        console.log("String is  not Palidrome ")
+      }       
+   } 
+
+   checkPalidromeString("raukll")
